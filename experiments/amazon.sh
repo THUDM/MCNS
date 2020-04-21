5e1a8298de322e3f345239104ac1e1f8c9fda0cc
ZYang16 <34953056+zyang-16@users.noreply.github.com>
2020-04-21 21:59:02 +0800

    Update amazon.sh

--- experiments/amazon.sh
+++ experiments/amazon.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0
-python main.py --input ./data/amazon/ --user_num 192403 --item_num 63001 --save_dir ./embeddings_amazon/
+python main.py --input ./data/amazon/ --batch_size 20 --user_num 192403 --item_num 63001 --save_dir ./embeddings_amazon/
