5b5b72109623ec5e3be26855230bd8d6efa91f45
ZYang16 <34953056+zyang-16@users.noreply.github.com>
2020-05-27 10:45:42 +0800

    Update ml.sh

--- experiments/ml.sh
+++ experiments/ml.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=3
-python main.py --input ./data/ml-100k/ --epochs 100 --batch_size 256 --validate_batch_size 256 --dim_1 128 --dim_2 128 --user_num 943 --item_num 1682 --patience 50 --save_dir ./embeddings_ml/
+python main.py --input ./data/ml-100k/ --epochs 100 --batch_size 256 --validate_batch_size 256 --samples_1 50 --samples_2 20 --dim_1 128 --dim_2 128 --max_degree 500 --user_num 943 --item_num 1682 --patience 50 --save_dir ./embeddings_ml/
