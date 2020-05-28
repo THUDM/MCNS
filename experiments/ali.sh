3c94b2efcd65a5b360d4ff9263a5ca913c13943a
ZYang16 <34953056+zyang-16@users.noreply.github.com>
2020-05-28 08:42:07 +0800

    Update ali.sh

--- experiments/ali.sh
+++ experiments/ali.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=2
-python main.py --input ./data/ali/ --epochs 50 --patience 20 --user_num 106042 --item_num 53591 --save_dir ./embeddings_ali/
+python main.py --input ./data/ali/ --epochs 30 --patience 20 --user_num 106042 --item_num 53591 --save_dir ./embeddings_ali/
