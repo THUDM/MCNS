af7c976f4b677252318246177f6b72a761e9261a
ZYang16 <34953056+zyang-16@users.noreply.github.com>
2020-06-24 11:23:54 +0800

    Update gcn.sh

--- experiments/gcn.sh
+++ experiments/gcn.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=3
-python main_gcn.py --input ./data/ml-100k/ --model gcn --epochs 100 --batch_size 256 --validate_batch_size 256 --dim 256 --user_num 943 --item_num 1682 --patience 50 --save_dir ./embeddings_ml/
+python main_gcn.py --input ./data/ml-100k/ --model gcn --epochs 200 --batch_size 256 --validate_batch_size 256 --dim 256 --user_num 943 --item_num 1682 --patience 100 --save_dir ./embeddings_ml/
