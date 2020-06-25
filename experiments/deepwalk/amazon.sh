51eea3ca026bdd1b1790f97dac7bfc3fdf3eabb2
ZYang16 <34953056+zyang-16@users.noreply.github.com>
2020-06-25 09:55:08 +0800

    Update amazon.sh

--- experiments/deepwalk/amazon.sh
+++ experiments/deepwalk/amazon.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=7
-python main.py --input ./data/amazon/ --model deepwalk --epochs 30 --patience 20 --user_num 192403 --item_num 63001 --save_dir ./embeddings_amazon/
+python main.py --input ./data/amazon/ --model deepwalk --epochs 25 --patience 20 --user_num 192403 --item_num 63001 --save_dir ./embeddings_amazon/
