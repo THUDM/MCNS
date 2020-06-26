fefd35224c6b26ed156794823ad5c68a286761c5
zyang-16 <13070106218@163.com>
2020-06-26 21:59:11 +0800

    train

--- experiments/deepwalk/ali.sh
+++ experiments/deepwalk/ali.sh
@@ -1,2 +1,2 @@
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=7
 python main.py --input ./data/ali/ --model deepwalk --epochs 15 --batch_size 64 --validate_batch_size 64 --dim 512 --patience 10 --user_num 106042 --item_num 53591 --save_dir ./embeddings_ali/
--- experiments/graphsage/amazon.sh
+++ experiments/graphsage/amazon.sh
@@ -1,2 +1,2 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 python main.py --input ./data/amazon/ --model graphsage_mean --epochs 30 --patience 20 --user_num 192403 --item_num 63001 --save_dir ./embeddings_amazon/
